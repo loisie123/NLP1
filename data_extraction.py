01c74e1718888f78c8878b11b4e4174c8f4f74eb
koenderks <koen-derks@hotmail.com>
2017-11-27 17:08:05 +0100

    added readWordEmbeddings.py

--- data_extraction.py
+++ data_extraction.py
@@ -1,27 +1,25 @@
 from conllu.parser import parse, parse_tree
+import re
+
+data = []
 
 with open('en-ud-dev.txt', 'r') as myfile:
-    data=myfile.read().replace('\n', '')
+    data=myfile.readlines()
 
+hoi = []
+
+for row in data:
+    if not (not row):
+        print(row)
+        if not (row.split()[0] == "#"):
+            hoi.append(row)
+
+print(hoi)
+
+data = re.sub(r" +", r"\t", data)
 # print(data)
 
-# data = """
-# 1   The the DET DT  Definite=Def|PronType=Art   4   det _   _
-# 2   quick   quick   ADJ JJ  Degree=Pos  4   amod    _   _
-# 3   brown   brown   ADJ JJ  Degree=Pos  4   amod    _   _
-# 4   fox fox NOUN    NN  Number=Sing 5   nsubj   _   _
-# 5   jumps   jump    VERB    VBZ Mood=Ind|Number=Sing|Person=3|Tense=Pres|VerbForm=Fin   0   root    _   _
-# 6   over    over    ADP IN  _   9   case    _   _
-# 7   the the DET DT  Definite=Def|PronType=Art   9   det _   _
-# 8   lazy    lazy    ADJ JJ  Degree=Pos  9   amod    _   _
-# 9   dog dog NOUN    NN  Number=Sing 5   nmod    _   SpaceAfter=No
-# 10  .   .   PUNCT   .   _   5   punct   _   _
-#
-# """
-
-# import re
-# data = re.sub(r" +", r"\t", data)
-
 results = parse(data)
+
 print("results =")
-print(results)
+# print(results)
---
+++ readWordEmbeddings.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+word_embeddings_50d = pd.read_csv('/home/koen/Documents/NaturalLanguageProcessing/Project/glove.6B.50d.txt', sep=" ", header=None, quotechar = "'")
+word_embeddings_100d = pd.read_csv('/home/koen/Documents/NaturalLanguageProcessing/Project/glove.6B.100d.txt', sep=" ", header=None, quotechar = "'")
+word_embeddings_200d = pd.read_csv('/home/koen/Documents/NaturalLanguageProcessing/Project/glove.6B.200d.txt', sep=" ", header=None, quotechar = "'")
+word_embeddings_300d = pd.read_csv('/home/koen/Documents/NaturalLanguageProcessing/Project/glove.6B.300d.txt', sep=" ", header=None, quotechar = "'")
