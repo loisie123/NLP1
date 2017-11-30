f0ea4358c61f0cb4de9d34d0bf622a773656f1e3
koenderks <koen-derks@hotmail.com>
2017-11-30 13:06:09 +0100

    fixed some stuff in readWordEmbeddings

--- readWordEmbeddings.py
+++ readWordEmbeddings.py
@@ -1,6 +1,7 @@
 import pandas as pd
+import tqdm
 
-word_embeddings_50d = pd.read_csv('/home/koen/Documents/NaturalLanguageProcessing/Project/glove.6B.50d.txt', sep=" ", header=None, quotechar = "'")
-word_embeddings_100d = pd.read_csv('/home/koen/Documents/NaturalLanguageProcessing/Project/glove.6B.100d.txt', sep=" ", header=None, quotechar = "'")
-word_embeddings_200d = pd.read_csv('/home/koen/Documents/NaturalLanguageProcessing/Project/glove.6B.200d.txt', sep=" ", header=None, quotechar = "'")
-word_embeddings_300d = pd.read_csv('/home/koen/Documents/NaturalLanguageProcessing/Project/glove.6B.300d.txt', sep=" ", header=None, quotechar = "'")
+word_embeddings_50d = pd.DataFrame(pd.read_csv('/home/koen/Documents/NaturalLanguageProcessing/Project/glove.6B.50d.txt', sep=" ", header=None, quotechar = "'"))
+word_embeddings_100d = pd.DataFrame(pd.read_csv('/home/koen/Documents/NaturalLanguageProcessing/Project/glove.6B.100d.txt', sep=" ", header=None, quotechar = "'"))
+word_embeddings_200d = pd.DataFrame(pd.read_csv('/home/koen/Documents/NaturalLanguageProcessing/Project/glove.6B.200d.txt', sep=" ", header=None, quotechar = "'"))
+word_embeddings_300d = pd.DataFrame(pd.read_csv('/home/koen/Documents/NaturalLanguageProcessing/Project/glove.6B.300d.txt', sep=" ", header=None, quotechar = "'"))
